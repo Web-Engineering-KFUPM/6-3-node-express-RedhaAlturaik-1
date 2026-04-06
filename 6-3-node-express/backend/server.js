6f4bffef0d56e1f250722a0cc0084931fee5cd8d
RedhaAlturaik <s202323010@kfupm.edu.sa>
2026-04-06 08:29:16 +0300

    Complete lab exercise: [brief description]

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,18 +1,30 @@
 // TODO 1: Import and Create express app instance
-
-// TODO 1: Define server port
-
+import express from "express";
+const app = express();
+// TODO 1: Definev server port
+const PORT = 3000;
 
 // TODO 4: Import and Apply CORS middleware
-
+import cors from "cors";
+app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
-
+import morgan from "morgan";
+app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
-
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
 
 // TODO 6.2: Create "/api/quote" route
-
+import { getRandomQuote } from "./quotes.js";
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 
 // TODO 7: Start server using app.listen
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
